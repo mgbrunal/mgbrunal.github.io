e08780ef7ffc64ca819060944f31413d3e0a5403
maugarbru <mauriciogarzonb@gmail.com>
2023-12-13 11:00:46 -0500

    copy astro repo here

---
+++ tailwind.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: ["row-span-", "col-span-"],
+  theme: {
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "white",
+          },
+        },
+      },
+      animation: {
+        typing: "typing 3s steps(30) infinite alternate, blink .7s infinite",
+      },
+    },
+  },
+  plugins: [],
+};
