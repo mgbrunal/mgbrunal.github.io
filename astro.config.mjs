baf8d5f8085afa0082b170599305b178a8032503
MGarzon TouchTo <mgarzon@touchto.io>
2025-06-04 09:39:16 -0500

    fix(deploy) fix deploy try #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
   site: 'https://mgbrunal.github.io',
   integrations: [tailwind()]
 });
