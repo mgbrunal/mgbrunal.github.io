d8dfaa0802effb97a9d59319bdeeeeb39f1d750b
M.Garzon <mauriciogarzonb@gmail.com>
2026-01-16 19:30:49 -0500

    refactor(app) updates astro, redefine site for business

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   safelist: ["row-span-", "col-span-"],
   theme: {
+    colors: {
+      white: "#FFF",
+      black: "#000",
+      primary: "#0C1A1A",
+      secondary: "#6ACFC7",
+      tertiary: "#FF6B4A",
+    },
     extend: {
       keyframes: {
         typing: {
