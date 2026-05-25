d4411e1d8c88fc22d86565129c0d562000741f90
Mauricio Garzon <mauriciogarzonb@gmail.com>
2026-05-25 12:01:18 -0500

    phase 1 design polish

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -11,34 +13,33 @@ export default {
       tertiary: "#BAFF39",
     },
     extend: {
+      transitionTimingFunction: {
+        "out-strong": "cubic-bezier(0.23, 1, 0.32, 1)",
+        "in-out-strong": "cubic-bezier(0.77, 0, 0.175, 1)",
+      },
       keyframes: {
         typing: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden",
-          },
-          "100%": {
-            width: "100%",
-          },
+          "0%": { width: "0%", visibility: "hidden" },
+          "100%": { width: "100%" },
         },
         blink: {
-          "50%": {
-            borderColor: "transparent",
-          },
-          "100%": {
-            borderColor: "white",
-          },
+          "50%": { borderColor: "transparent" },
+          "100%": { borderColor: "white" },
         },
-        'text-slider': {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-50%)' },
+        "text-slider": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
         },
       },
       animation: {
         typing: "typing 3s steps(30) infinite alternate, blink .7s infinite",
-        'text-slider': 'text-slider 20s linear infinite',
+        "text-slider": "text-slider 20s linear infinite",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant }) => {
+      addVariant("can-hover", "@media (hover: hover) and (pointer: fine)");
+    }),
+  ],
 };
